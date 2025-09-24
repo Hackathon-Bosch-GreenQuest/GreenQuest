1392652db12f4f360e781d94610cbb25bddc8da7
Murilo M. Zanetti <70murilomz@gmail.com>
2025-09-24 02:51:37 -0300

    feat: alteração na inicialização do firebase

--- frontend/GreenQuest/src/services/firebaseConfig.js
+++ frontend/GreenQuest/src/services/firebaseConfig.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import firebase from '@react-native-firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,10 +11,10 @@ const firebaseConfig = {
   projectId: "green-quest-563ad",
   storageBucket: "green-quest-563ad.firebasestorage.app",
   messagingSenderId: "978170249277",
-  appId: "1:978170249277:web:7a4313ac4ee7eb3773aa54",
-  measurementId: "G-2K6KX2NM3H"
+  appId: "1:978170249277:web:7a4313ac4ee7eb3773aa54"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
